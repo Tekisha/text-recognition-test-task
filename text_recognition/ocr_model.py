401c8a7ac3a75234b73f5d19bc0bbb96c59e087f
Teodor Vidakovic <teodorv22@gmail.com>
2023-12-29 02:56:23 +0100

    feat: create recognize_text method

--- text_recognition/ocr_model.py
+++ text_recognition/ocr_model.py
@@ -1,10 +1,7 @@
 import os
 from pathlib import Path
-
 from pytesseract import pytesseract
 import cv2
-from langdetect import detect
-from easyocr import Reader
 
 
 class OCRModel:
@@ -12,19 +9,16 @@ class OCRModel:
         pytesseract.tesseract_cmd = '../tesseract/tesseract.exe'
         os.environ['TESSDATA_PREFIX'] = '../tesseract/tessdata'
         if languages is None:
-            languages = ['en', 'ch_sim']
-        self.reader = Reader(languages, gpu=True)
+            self.languages = "eng+chi_sim"
 
-    def image_first_scan(self, image: Path) -> str:
-        try:
-            result = ' '.join(self.reader.readtext(str(image), detail=0))
-            if not result:
-                return "No text found in the image."
-            return str(result)
-        except Exception as e:
-            print(f"Error processing image {image}: {e}")
-            return "An error occurred during text recognition."
+    def preprocess_image(self, image):
+        """
+        Apply advanced preprocessing to the image to improve OCR accuracy.
+        """
+        # Convert to grayscale
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+        return gray
 
     def recognize_text(self, image: Path) -> str:
         """
@@ -33,31 +27,13 @@ class OCRModel:
         :param image: The path to the image file.
         :return: The recognized text from the image.
         """
-        first_scan_text = self.image_first_scan(image)
-        detected_lang = self.detect_language(first_scan_text)
-        # Read the image using OpenCV
         image = cv2.imread(str(image))
 
-        # Convert the image to grayscale
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        preprocessed_image = self.preprocess_image(image)
 
-        text = pytesseract.image_to_string(gray_image, lang='eng+chi_sim', config='--oem 1 --psm 6 -c preserve_interword_spaces=1')
+        text = pytesseract.image_to_string(preprocessed_image, lang=self.languages, config='--oem 1 --psm 6 -c preserve_interword_spaces=1')
 
         return text.strip()
-
-    def detect_language(self, text: str) -> str:
-        try:
-            language = detect(text)
-
-            lang_mapping = {
-                'en': 'eng',
-                'zh-cn': 'chi_sim',
-                # Add more mappings as needed
-            }
-
-            return lang_mapping.get(language)
-        except:
-            return "eng"
 
 
 if __name__ == "__main__":
